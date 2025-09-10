61f04f09e0eed6e5fec12e60ce4da3a4a30e82f7
yassun32 <yassinblailastor15@gmail.com>
2025-09-10 16:27:45 +0100

    many addition

---
+++ sas.c
@@ -0,0 +1,22 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include<string.h>
+void affichageList(){
+}
+void modification(){
+}
+void supprition(){
+}
+void recherche(){
+}
+void statistique(){
+}
+
+int main (){
+    affichageList();
+    modification();
+    supprition();
+    recherche();
+    statistique();
+    return 0;
+}
